cc421f1b9afa59f4fcdc1df88921b76d255ffbd8
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-06-10 18:19:15 -0500

    fix moment.js link

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@ localStorage.clear();
 function findCity() {
     var cityName = titleCase($("#cityName")[0].value.trim());
 
-    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=71311474f5b26fb7bbfa0bc1985b90cd";
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + 
+    "&units=imperial&appid=71311474f5b26fb7bbfa0bc1985b90cd";
 
     fetch(apiURL).then(function (response) {
         if (response.ok) {
